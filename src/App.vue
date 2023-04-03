7e938e5bb37fb0c0823d959453068ff94166aa75
yqiuc <98722550+yqiuc@users.noreply.github.com>
2023-04-03 15:26:55 -0500

    update2

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,108 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
-</template>
 
-<style>
+  <ul class="nav justify-content-center">
+    <li class="nav-item active">
+      <router-link to="/">Home</router-link> |
+    </li>
+    <li class="nav-item">
+      <router-link :to="{name: 'MovieList'}">Movies</router-link>
+    </li>
+    <li class="nav-item" v-if="!authenticated" @click="login" >
+      | <router-link :to="{name: 'Auth'}">Log in</router-link>
+    </li>
+    <li class="nav-item" v-if="!authenticated" @click="register" >
+      | <router-link :to="{name: 'Register'}">Register</router-link>
+    </li>
+    <li class="nav-item .justify-content-end" v-if="authenticated" @click="logout" >
+      | <router-link :to="{name: 'Auth'}">Logout</router-link>
+    </li>
+  </ul>
+
+  <router-view/>
+  </template>
+
+<script>
+
+import router from "./router";
+import { APIService } from "./http/APIService";
+const apiService = new APIService();
+
+export default {
+  name: 'App',
+  props: {
+    msg: String
+  },
+  data: () => ({
+    authenticated: false,
+    dialog: false,
+    uName: "Guest",
+    menu: [],
+  }),
+  mounted() {
+    this.fillMenu();
+    apiService
+        .getMovieList()
+        .then((response) => {
+          this.authenticated = true;
+        })
+        .catch((error) => {
+          if (error.response.status === 401) {
+            localStorage.removeItem("isAuthenticates");
+            localStorage.removeItem("log_user");
+            localStorage.removeItem("token");
+            this.authenticated = false;
+          }
+        });
+    console.log("this.authenticated--" + this.authenticated);
+    },
+  methods: {
+    fillMenu() {
+      if (
+          localStorage.getItem("isAuthenticates") &&
+          JSON.parse(localStorage.getItem("isAuthenticates")) === true
+      ) {
+        console.log("IS AUTH");
+        this.uName = JSON.parse(localStorage.getItem("log_user"));
+      } else {
+        console.log("IS NOT AUTH");
+      }
+      this.menu = [{ title: "Movies", url: "/movie-list" }];
+    },
+    getUser() {
+      if (
+          localStorage.getItem("isAuthenticates") &&
+          JSON.parse(localStorage.getItem("isAuthenticates")) === true
+      ) {
+        console.log("IS AUTH");
+        this.validUserName = JSON.parse(localStorage.getItem("log_user"));
+      } else {
+        console.log("IS NOT AUTH");
+      }
+    },
+    logout() {
+      localStorage.removeItem("isAuthenticates");
+      localStorage.removeItem("log_user");
+      localStorage.removeItem("token");
+      this.authenticated = false;
+      // router.push('/');
+      window.location = "/";
+    },
+    home() {
+      window.location = "/";
+    },
+    register() {
+      router.push("/register");
+    },
+    login() {
+      router.push("/auth");
+    },
+  },
+}
+</script>
+
+
+<style lang="scss">
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -15,16 +111,36 @@
   color: #2c3e50;
 }
 
-nav {
+#nav {
   padding: 30px;
+  background-color: cadetblue;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+.nav {
+  padding: 1em;
+  background-color: cadetblue;
 
-nav a.router-link-exact-active {
-  color: #42b983;
+  li {
+    font-weight: bold;
+    color: #2c3e50;
+  }
+
+  a {
+    color: black;
+    padding: .5em;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,49 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../views/Home.vue'
+import MovieList from '../components/MovieList'
+import MovieCreate from '../components/MovieCreate'
+import Auth from '../components/Auth'
+import Register from '../components/Register'
+
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Home',
+    component: Home
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/movie-list',
+    name: 'MovieList',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component:MovieList,
+  },
+  {
+    path:'/movie-list/:msg',
+    name: 'MovieUpdatedList',
+    component: MovieList
+  },
+  {
+    path:'/movie-create',
+    name:'MovieCreate',
+    component: MovieCreate
+  },
+  {
+    path: '/movie-create/:pk',
+    name: 'MovieUpdate',
+    component: MovieCreate
+  },
+  {
+    path:'/auth',
+    name:'Auth',
+    component: Auth
+  },
+  {
+    path:'/register',
+    name: 'Register',
+    component: Register
   }
 ]
 
@@ -21,5 +51,14 @@ const router = createRouter({
   history: createWebHashHistory(),
   routes
 })
+
+// {
+// path: '/about',
+// name: 'About',
+// // route level code-splitting
+// // this generates a separate chunk (about.[hash].js) for this route
+// // which is lazy-loaded when the route is visited.
+// component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+// },
 
 export default router
